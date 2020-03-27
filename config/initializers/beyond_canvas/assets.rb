e90aa351725f28417d76c0256dc733c62c9982c0
Unai Abrisketa <u.abrisketa@epages.com>
2020-03-27 16:15:54 +0100

    Revert "Release v0.11.0.pre"
    This reverts commit 96e495b033cbe4a92f13d2d8c96fe20db0ab83e6.

--- config/initializers/beyond_canvas/assets.rb
+++ config/initializers/beyond_canvas/assets.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
-Rails.application.config.assets.precompile += %w( beyond_canvas/mailer.css )
 Rails.application.config.assets.precompile += %w( beyond_canvas.css )
 Rails.application.config.assets.precompile += %w( beyond_canvas.js )
