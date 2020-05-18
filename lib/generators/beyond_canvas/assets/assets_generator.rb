df092a2c554ac160a1f5453b84abcc0328a05275
unabris <u.abrisketa@epages.com>
2020-05-18 11:32:30 +0200

    Add asset generator

---
+++ lib/generators/beyond_canvas/assets/assets_generator.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module BeyondCanvas
+  module Generators
+    class AssetsGenerator < Rails::Generators::Base
+
+      source_root File.expand_path('templates', __dir__)
+
+      def install_assets
+        template 'beyond_canvas.js', 'app/assets/javascripts/beyond_canvas.js'
+        template 'beyond_canvas.scss', 'app/assets/stylesheets/beyond_canvas.scss'
+      end
+
+    end
+  end
+end
