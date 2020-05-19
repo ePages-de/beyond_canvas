8a0d6a64ea89560c2cd5bc395601c1b53d72b600
unabris <u.abrisketa@epages.com>
2020-05-19 11:34:02 +0200

    Use svg instead of fontawesome

--- app/javascript/beyond_canvas/initializers/inputs.js
+++ app/javascript/beyond_canvas/initializers/inputs.js
@@ -17,7 +17,7 @@
 
         if (fileName)
           $label.html(
-            `<i class="far fa-file input__file__icon"></i>${fileName}`
+            `<svg class="input__file__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15 2v5h5v15h-16v-20h11zm1-2h-14v24h20v-18l-6-6z"/></svg>${fileName}`
           );
         else $label.html(labelVal);
       });
