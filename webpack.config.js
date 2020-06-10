d860cc2f54230bd2019032f7020f4910ab7c9e1d
gersanco <g.sanemeterio@epages.com>
2020-06-10 15:42:14 +0200

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const webpack = require('webpack');
+const BANNER = `
+   Warning: This file is auto-generated, do not modify. Instead, make your changes in 'app/javascript/beyond_canvas/' and run \`yarn build\`
+
+   //= require jquery3
+   //= require_self
+  `;
+
+module.exports = {
+  mode: 'production',
+  entry: [
+    './app/javascript/beyond_canvas/base.js',
+    './app/javascript/beyond_canvas/initializers/functions.js',
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  target: 'web',
+  output: {
+    filename: 'base.js',
+    path: path.resolve(__dirname, 'app/assets/javascripts/beyond_canvas'),
+    libraryTarget: 'global',
+  },
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: BANNER,
+    }),
+  ],
+  externals: {
+    $: 'jquery',
+    jQuery: 'jquery',
+  },
+};
