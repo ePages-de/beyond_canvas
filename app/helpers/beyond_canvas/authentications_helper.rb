a04aa9b1397e0d7f95181c9192dbc4561c3bedb5
Unai Abrisketa <u.abrisketa@epages.com>
2020-08-27 11:47:49 +0200

    Better look of comments

--- app/helpers/beyond_canvas/authentications_helper.rb
+++ app/helpers/beyond_canvas/authentications_helper.rb
@@ -2,19 +2,25 @@
 
 module BeyondCanvas
   module AuthenticationsHelper # :nodoc:
+    #
     # Logs in the given shop
+    #
     def log_in(shop)
       session[:shop_id] = shop.id
     end
 
+    #
     # Returns the current logged-in shop (if any)
+    #
     def current_shop
       if session[:shop_id]
         @current_shop ||= Shop.find_by(id: session[:shop_id])
       end
     end
 
+    #
     # Returns true if the shop is logged in, false otherwise
+    #
     def logged_in?
       !current_shop.nil?
     end
