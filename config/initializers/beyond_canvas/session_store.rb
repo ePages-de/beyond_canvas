ce23b15189de816c7735e70b0f5044b8410f5ac2
gersanco <g.sanemeterio@epages.com>
2021-02-05 15:33:05 +0100

    Check if it's a cockpit app and not in a development env

--- config/initializers/beyond_canvas/session_store.rb
+++ config/initializers/beyond_canvas/session_store.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if BeyondCanvas.configuration.cockpit_app == true && Rails.env.production?
+if BeyondCanvas.configuration.cockpit_app == true && !Rails.env.development?
   Rails.application.config.session_store :cookie_store, {
     secure: :true,
     same_site: :none
