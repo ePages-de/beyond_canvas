b977b93202c089c7f428826a6c64b453bee23fc7
gersanco <g.sanemeterio@epages.com>
2021-03-02 14:23:46 +0100

    print session loaded

--- app/helpers/beyond_canvas/authentications_helper.rb
+++ app/helpers/beyond_canvas/authentications_helper.rb
@@ -15,7 +15,7 @@ module BeyondCanvas
     def current_shop
       puts '*' * 75
       puts session[:shop_id]
-      puts session
+      puts session.loaded?
       puts '*' * 75
       if session[:shop_id]
         @current_shop ||= Shop.find_by(id: session[:shop_id])
