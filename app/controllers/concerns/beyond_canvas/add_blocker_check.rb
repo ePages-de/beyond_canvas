b9c011f25496684712e991e726c5dc132975e9cb
gersanco <g.sanemeterio@epages.com>
2021-03-11 10:57:55 +0100

    Comments

--- app/controllers/concerns/beyond_canvas/add_blocker_check.rb
+++ app/controllers/concerns/beyond_canvas/add_blocker_check.rb
@@ -6,11 +6,24 @@ module BeyondCanvas
 
     included do
       before_action :check_session_availability
+      after_action :after_check_session_availability
     end
 
     private
 
     def check_session_availability
+      puts '#' * 100
+      puts 'before_check_session_availability'
+      puts session.loaded?
+      puts '#' * 100
+      # redirect_to '/disable_add_blocker.html' unless session.loaded?
+    end
+
+    def after_check_session_availability
+      puts '~' * 100
+      puts 'after_check_session_availability'
+      puts session.loaded?
+      puts '~' * 100
       # redirect_to '/disable_add_blocker.html' unless session.loaded?
     end
   end
