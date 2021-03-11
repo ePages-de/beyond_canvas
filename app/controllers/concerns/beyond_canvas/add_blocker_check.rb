846b6d445ff7d057ace4bd2748866d3967def36e
gersanco <g.sanemeterio@epages.com>
2021-03-11 09:04:42 +0100

    Put comments

--- app/controllers/concerns/beyond_canvas/add_blocker_check.rb
+++ app/controllers/concerns/beyond_canvas/add_blocker_check.rb
@@ -11,6 +11,9 @@ module BeyondCanvas
     private
 
     def check_session_availability
+      puts '*' * 75
+      puts session.loaded?
+      puts '*' * 75
       redirect_to '/disable_add_blocker.html' unless session.loaded?
     end
   end
