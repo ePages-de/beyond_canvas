b26c5461e0eff93b98993f9f6718fe7c021b7dd6
gersanco <g.sanemeterio@epages.com>
2021-03-11 16:43:29 +0100

    Not check_session_availability when is beyond_canvas controller

--- app/controllers/concerns/beyond_canvas/add_blocker_check.rb
+++ app/controllers/concerns/beyond_canvas/add_blocker_check.rb
@@ -6,28 +6,12 @@ module BeyondCanvas
 
     included do
       before_action :check_session_availability, unless: -> { controller_path == 'beyond_canvas/authentications' }
-      # after_action :after_check_session_availability
     end
 
     private
 
     def check_session_availability
-      puts '#' * 160
-      puts 'before_check_session_availability'
-      puts session.loaded?
-      puts controller_name
-      puts controller_path
-      puts '#' * 160
       redirect_to '/disable_add_blocker.html' unless session.loaded?
-    end
-
-    def after_check_session_availability
-      puts '~' * 100
-      puts 'after_check_session_availability'
-      puts session.loaded?
-      puts controller_name
-      puts '~' * 100
-      # redirect_to '/disable_add_blocker.html' unless session.loaded?
     end
   end
 end
