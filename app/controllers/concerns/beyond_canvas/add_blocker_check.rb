d757b0075f38d5c6e29cca65b353b3e10363e168
Unai Abrisketa <u.abrisketa@epages.com>
2021-03-24 12:28:14 +0100

    Disable session availability

--- app/controllers/concerns/beyond_canvas/add_blocker_check.rb
+++ app/controllers/concerns/beyond_canvas/add_blocker_check.rb
@@ -4,14 +4,14 @@ module BeyondCanvas
   module AddBlockerCheck # :nodoc:
     extend ActiveSupport::Concern
 
-    included do
-      before_action :check_session_availability, unless: -> { controller_path == 'beyond_canvas/authentications' }
-    end
+    # included do
+    #   before_action :check_session_availability, unless: -> { controller_path == 'beyond_canvas/authentications' }
+    # end
 
-    private
+    # private
 
-    def check_session_availability
-      redirect_to '/disable_add_blocker.html' unless session.loaded?
-    end
+    # def check_session_availability
+    #   redirect_to '/disable_add_blocker.html' unless session.loaded?
+    # end
   end
 end
