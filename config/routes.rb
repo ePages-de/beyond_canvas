9b9c9e0e4d8a00d02c79a452442002fd38d6f97b
gersanco <g.sanemeterio@epages.com>
2021-05-27 12:26:32 +0200

    Add webhook controller to listen to handle beyond webhooks

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ BeyondCanvas::Engine.routes.draw do
   def create_default_routes
     get  'callback', to: 'authentications#new'
     post 'callback', to: 'authentications#install'
+
+    resources :shops do
+      member do
+        post :beyond_webhook, to: 'beyond_webhook#webhook'
+      end
+    end
   end
 
   create_default_routes unless BeyondCanvas.use_rails_app_controller
