b4cf6eecbb949827a9eab76e1dfd918aae5d8e3a
gersanco <g.sanemeterio@epages.com>
2021-08-03 13:12:31 +0200

    Show text by default when upload multiple files

--- app/javascript/beyond_canvas/initializers/inputs.js
+++ app/javascript/beyond_canvas/initializers/inputs.js
@@ -1,15 +1,15 @@
 (function ($) {
   const onDOMReady = function () {
     $('input[type="file"]').each(function () {
-      var $input = $(this),
-        $label = $(`.input__file__text.${$input.attr('id')}`),
-        labelVal = $label.html();
+      const $input = $(this),
+            $label = $(`.input__file__text.${$input.attr('id')}`),
+            labelVal = $label.html();
 
       $input.on('change', function (e) {
-        var fileName = '';
+        let fileName = '';
 
         if (this.files && this.files.length > 1)
-          fileName = (this.getAttribute('data-multiple-caption') || '').replace(
+          fileName = (this.getAttribute('data-multiple-caption') || '{count} files selected').replace(
             '{count}',
             this.files.length
           );
