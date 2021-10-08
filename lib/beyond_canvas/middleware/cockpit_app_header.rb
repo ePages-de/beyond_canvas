271f19657408e0b304e55755a14f7d907c49d9f4
gersanco <g.sanemeterio@epages.com>
2021-10-08 10:15:44 +0200

    Add logs to middleware

--- lib/beyond_canvas/middleware/cockpit_app_header.rb
+++ lib/beyond_canvas/middleware/cockpit_app_header.rb
@@ -6,6 +6,9 @@ module BeyondCanvas
       end
 
       def call(env)
+        puts '~' * 75
+        puts 'INSIDE MIDDLEWARE'
+        puts '~' * 75
         status, headers, response = @app.call(env)
         headers['X-Frame-Options'] = 'ALLOWALL'
         [status, headers, response]
