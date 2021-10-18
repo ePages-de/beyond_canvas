0846eae10ec5675c1021a10888d8f538c976b42d
gersanco <g.sanemeterio@epages.com>
2021-10-18 08:37:56 +0200

    Allow all for *

--- lib/beyond_canvas/middleware/cockpit_app_header.rb
+++ lib/beyond_canvas/middleware/cockpit_app_header.rb
@@ -9,6 +9,11 @@ module BeyondCanvas
         status, headers, response = @app.call(env)
         request = ActionDispatch::Request.new env
 
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS, PATCH, DELETE'
+        headers['Access-Control-Request-Method'] = '*'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Frame-Options, X-CSRF-Token'
+
         headers['X-Frame-Options'] = 'ALLOWALL'
 
         [status, headers, response]
