4dba64d20197ea39d5b109984da25bf3f50b26af
gersanco <g.sanemeterio@epages.com>
2021-10-20 09:15:16 +0200

    Configure security policy for frame ancestors

--- lib/beyond_canvas/middleware/cockpit_app_header.rb
+++ lib/beyond_canvas/middleware/cockpit_app_header.rb
@@ -9,30 +9,11 @@ module BeyondCanvas
         status, headers, response = @app.call(env)
         request = ActionDispatch::Request.new env
 
-        # headers['X-Frame-Options'] = 'ALLOWALL' # Some browsers like Firefox needs this to display the page correctly in the iframe
-
-        puts request.headers['Sec-Fetch-Dest']
-
-        # Referer
-        # Sec-Fetch-Dest
-
         if request.headers['Sec-Fetch-Dest'] == 'iframe'
           headers['Content-Security-Policy'] = <<~POLICY.gsub "\n", ' '
             frame-ancestors #{request.referer};
           POLICY
-
-          puts '-' * 75
-          puts request.referer
-          puts request.headers['referer']
-        puts '-' * 75
         end
-
-        # unless request.user_agent.match(/Chrome/)
-        #   headers['Access-Control-Allow-Origin'] = '*'
-        #   headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS, PATCH, DELETE'
-        #   headers['Access-Control-Request-Method'] = '*'
-        #   headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Frame-Options, X-CSRF-Token'
-        # end
 
         [status, headers, response]
       end
