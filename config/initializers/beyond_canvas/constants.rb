3298a9db396cb08519aad97abaa34c2b79979c16
gersanco <g.sanemeterio@epages.com>
2021-10-22 10:22:16 +0200

    Add logs on constant

--- config/initializers/beyond_canvas/constants.rb
+++ config/initializers/beyond_canvas/constants.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 if BeyondCanvas.configuration.cockpit_app == true && !Rails.env.development?
+  puts 'INSIDE SECURE NONE'
   COOKIES_ATTRIBUTES = {
     expires: 1.year.from_now,
     same_site: :none,
     secure: true
   }.freeze
 else
+  puts 'OUTSIDE SECURE NONE'
   COOKIES_ATTRIBUTES = {
     expires: 1.year.from_now
   }.freeze
