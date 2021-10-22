82378a01114ba7b7f7c4f4d46dc85250a725add2
gersanco <g.sanemeterio@epages.com>
2021-10-22 10:30:51 +0200

    Add logs

--- config/initializers/beyond_canvas/constants.rb
+++ config/initializers/beyond_canvas/constants.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+puts "BeyondCanvas.configuration.cockpit_app #{BeyondCanvas.configuration.cockpit_app}"
+puts "!Rails.env.development? #{!Rails.env.development?}"
 if BeyondCanvas.configuration.cockpit_app == true && !Rails.env.development?
   puts 'INSIDE SECURE NONE'
   COOKIES_ATTRIBUTES = {
