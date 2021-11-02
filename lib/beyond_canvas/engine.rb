b780ff7fbeeff13035afaa9defb9b90bb63e9200
gersanco <g.sanemeterio@epages.com>
2021-11-02 15:35:44 +0100

    Use rails cache store to check if works with safari

--- lib/beyond_canvas/engine.rb
+++ lib/beyond_canvas/engine.rb
@@ -19,7 +19,7 @@ module BeyondCanvas
 
     initializer 'beyond_canvas.session' do |app|
       if BeyondCanvas.configuration.cockpit_app == true && Rails.env.production?
-        app.config.session_store :cookie_store, {
+        app.config.session_store :cache_store, {
           secure: true,
           same_site: :none,
         }
