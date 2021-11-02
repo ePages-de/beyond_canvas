f8281bf8bf5fdf1855a92a9362c532cb314895b1
gersanco <g.sanemeterio@epages.com>
2021-11-02 12:46:13 +0100

    Add policy when is iFrame or it's safari

--- lib/beyond_canvas/middleware/cockpit_app_header.rb
+++ lib/beyond_canvas/middleware/cockpit_app_header.rb
@@ -8,12 +8,13 @@ module BeyondCanvas
       def call(env)
         status, headers, response = @app.call(env)
         request = ActionDispatch::Request.new env
+        sec_fetch_dest = request.headers['Sec-Fetch-Dest']
 
-        if request.headers['Sec-Fetch-Dest'] == 'iframe'
+        if sec_fetch_dest == 'iframe' || (request.user_agent.match?(/Safari/) && sec_fetch_dest.blank?)
+          headers['Content-Security-Policy'] = <<~POLICY.gsub "\n", ' '
+            frame-ancestors #{request.session[:iframe_ancestor_url]} #{request.referer};
+          POLICY
         end
-        headers['Content-Security-Policy'] = <<~POLICY.gsub "\n", ' '
-          frame-ancestors #{request.session[:iframe_ancestor_url]} #{request.referer};
-        POLICY
 
         puts '-' * 75
 
