2aabefeaa4adae43c692f874fec53852aa419e79
gersanco <g.sanemeterio@epages.com>
2021-11-04 11:51:59 +0100

    Add logs

--- lib/beyond_canvas/middleware/cockpit_app_header.rb
+++ lib/beyond_canvas/middleware/cockpit_app_header.rb
@@ -18,6 +18,10 @@ module BeyondCanvas
         puts "Cockpit Sessions: #{request.session[:custom_styles_url]}"
         puts "Cockpit Sessions: #{request.session[:iframe_ancestor_url]}"
 
+        puts "Cockpit Cookies: #{request.cookies[:custom_styles_url]}"
+        puts "Cockpit Cookies: #{request.cookies[:locale]}"
+        puts "Cockpit Cookies: #{request.cookies[:shop_id]}"
+
         if sec_fetch_dest == 'iframe' || (request.user_agent.match?(/Safari/) && sec_fetch_dest.blank?)
           headers['Content-Security-Policy'] = <<~POLICY.gsub "\n", ' '
             frame-ancestors #{request.session[:iframe_ancestor_url]} #{request.referer};
