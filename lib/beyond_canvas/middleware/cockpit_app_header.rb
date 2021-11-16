b8238053f1a893c748ca329e88a3a23a92b49400
gersanco <g.sanemeterio@epages.com>
2021-11-16 11:29:42 +0100

    Remove logs

--- lib/beyond_canvas/middleware/cockpit_app_header.rb
+++ lib/beyond_canvas/middleware/cockpit_app_header.rb
@@ -10,47 +10,11 @@ module BeyondCanvas
         request = ActionDispatch::Request.new env
         sec_fetch_dest = request.headers['Sec-Fetch-Dest']
 
-        # puts '_' * 80
-        # puts "Cockpit Sessions: #{request.session}"
-        # puts "Cockpit Cookies:  #{request.cookies}"
-        # puts "Cockpit Headers:  #{request.session[:shop_id]}"
-        # puts "Cockpit Sessions: #{request.session[:locale]}"
-        # puts "Cockpit Sessions: #{request.session[:custom_styles_url]}"
-        # puts "Cockpit Sessions: #{request.session[:iframe_ancestor_url]}"
-
-        # puts "Cockpit Cookies: #{request.cookies}"
-        # puts "Cockpit Cookies: #{request.cookies[:custom_styles_url]}"
-        # puts "Cockpit Cookies: #{request.cookies[:locale]}"
-
-        # if sec_fetch_dest == 'iframe' || (request.user_agent.match?(/Safari/) && sec_fetch_dest.blank?)
+        if sec_fetch_dest == 'iframe' || (request.user_agent.match?(/Safari/) && sec_fetch_dest.blank?)
         headers['Content-Security-Policy'] = <<~POLICY.gsub "\n", ' '
           frame-ancestors #{request.session[:iframe_ancestor_url]} #{request.referer};
         POLICY
-        # end
-
-        # request.session[:safari_cookie_fixed] = true
-        # puts "Cockpit Sessions: #{request.session[:safari_cookie_fixed]}"
-
-        # puts "Cockpit Sessions: #{request.session[:iframe_ancestor_url]}"
-        # puts '_' * 80
-
-
-        # puts '-' * 75
-        # puts "CockpitAppHeader: #{request.path}"
-        # puts "CockpitAppHeader: #{request.headers['Sec-Fetch-Dest']}"
-        # puts "CockpitAppHeader: #{request.headers['Sec-Fetch-Site']}"
-        # puts "CockpitAppHeader: #{request.headers['Sec-Fetch-Mode']}"
-        # puts "CockpitAppHeader: #{request.headers['Sec-Fetch-User']}"
-        # puts "CockpitAppHeader: #{request.user_agent}"
-        # puts "CockpitAppHeader: #{request.headers['Accept']}"
-        # puts "CockpitAppHeader: #{request.headers['Accept-Encoding']}"
-        # puts "CockpitAppHeader: #{request.headers['Accept-Language']}"
-        # puts "CockpitAppHeader: #{request.headers['Cache-Control']}"
-        # puts "CockpitAppHeader: #{request.headers['Connection']}"
-        # puts "CockpitAppHeader: #{request.headers['Host']}"
-        # puts "CockpitAppHeader: #{request.headers['Origin']}"
-        # puts "CockpitAppHeader: #{request.headers['Referer']}"
-        # puts '-' * 75
+        end
 
         [status, headers, response]
       end
