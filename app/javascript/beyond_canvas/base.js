07d7da1b52e237e27746b3396b184e8a38e1d378
kengallego <kgallego@epages.com>
2022-02-23 14:20:48 +0100

    Change scrollbox to scroll-shadow

--- app/javascript/beyond_canvas/base.js
+++ app/javascript/beyond_canvas/base.js
@@ -4,3 +4,4 @@ import './initializers/collapse';
 import './initializers/flash';
 import './initializers/inputs';
 import './initializers/modals';
+import 'scroll-shadow-element/scroll-shadow-element';
