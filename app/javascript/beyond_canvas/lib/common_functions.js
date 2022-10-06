089a4978b966c28c29c6115406229fa5bf9fef75
German <g.sanemeterio@epages.com>
2022-10-06 14:04:34 +0200

    Image-tag-arguments (#92)
    * Add styles for icons on attachments class
    
    * Keep attributes after preview images
    
    * figure and image html options
    
    * Add cursor pointer
    
    Co-authored-by: gersanco <gsanemeterio@epages.com>

--- app/javascript/beyond_canvas/lib/common_functions.js
+++ app/javascript/beyond_canvas/lib/common_functions.js
@@ -4,6 +4,11 @@ export function previewImage(e) {
   const arr = Array.from(e.target.files);
   const elementFather = $(e.target).parents('.relative').find('.js-images');
 
+  const imgAttr = getAtrributesFromElement($(elementFather).find('figure').find('img')[0]);
+  const figureAttr = getAtrributesFromElement($(elementFather).find('figure')[0]);
+  delete imgAttr.src;
+  delete figureAttr.class;
+
   if($(e.target).attr('multiple')) {
     $(elementFather).children('attachment__placeholder, [preview]').each((_, img) => $(img).remove());
   } else {
@@ -22,8 +27,26 @@ export function previewImage(e) {
       figure.setAttribute('preview', true);
       img.setAttribute('src', reader.result);
 
+      setAttributesToElement(img, imgAttr);
+      setAttributesToElement(figure, figureAttr);
+
       $(figure).append(img);
       $(elementFather).append(figure);
     };
   });
 }
+
+export const getAtrributesFromElement = (element) => {
+  const attributes = {};
+  for (const attr of element.getAttributeNames()) {
+    attributes[attr] = element.getAttribute(attr);
+  }
+
+  return attributes;
+};
+
+export const setAttributesToElement = (element, attributes) => {
+  for (const attr in attributes) {
+    element.setAttribute(attr, attributes[attr]);
+  }
+};
